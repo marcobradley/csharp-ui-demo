0267222e450685757e654eca72016f886ee11596
marcobradley <marcobradley@users.noreply.github.com>
2026-03-28 15:02:40 -0400

    feat: fixing to post from the backend

--- csharp-ui/Pages/Index.cshtml.cs
+++ csharp-ui/Pages/Index.cshtml.cs
@@ -5,19 +5,31 @@ namespace csharp_ui.Pages;
 
 using csharp_ui.Helpers;
 
+public record PriceRequest(decimal[] prices);
+
 public class IndexModel : PageModel
 {
-    private readonly ILogger<IndexModel> _logger;
 
+    private readonly ILogger<IndexModel> _logger;
+    private readonly HttpClient _httpClient;
     public string ApiBaseUrl { get; set; }
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IHttpClientFactory httpClientFactory)
     {
         _logger = logger;
+        _httpClient = httpClientFactory.CreateClient();
     }
 
     public void OnGet()
     {
         ApiBaseUrl = ApiConfig.ApiBaseUrl;
+    }
+
+    public async Task<IActionResult> OnPostBestTimeToBuyOrSellStockAsync([FromBody] PriceRequest data)
+    {
+        var apiUrl = $"{ApiConfig.ApiBaseUrl}/besttimetobyorsellstock";
+        var response = await _httpClient.PostAsJsonAsync(apiUrl, data);
+        var result = await response.Content.ReadAsStringAsync();
+        return Content(result, "application/json");
     }
 }
