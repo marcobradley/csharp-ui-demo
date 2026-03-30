4a61883dfffe0c555005336f843788cc663a1c82
marcobradley <marcobradley@users.noreply.github.com>
2026-03-30 15:46:29 -0400

    fix: set base api urls

--- csharp-ui/Helpers/ApiConfig.cs
+++ csharp-ui/Helpers/ApiConfig.cs
@@ -1,12 +1,23 @@
 namespace csharp_ui.Helpers
 {
-    public static class ApiConfig
+    public static class GoApiConfig
     {
         public static string ApiBaseUrl
         {
             get
             {
-                var url = Environment.GetEnvironmentVariable("API_BASE_URL");
+                var url = Environment.GetEnvironmentVariable("GO_API_BASE_URL");
+                return string.IsNullOrEmpty(url) ? "http://localhost:8080" : url;
+            }
+        }
+    }
+    public static class CSharpApiConfig
+    {
+        public static string ApiBaseUrl
+        {
+            get
+            {
+                var url = Environment.GetEnvironmentVariable("CSHARP_API_BASE_URL");
                 return string.IsNullOrEmpty(url) ? "http://localhost:8080" : url;
             }
         }
